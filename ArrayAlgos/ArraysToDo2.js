fddac9e1a5039c5876fd1e617acd8dfcaba74d0f
Mortron <cmsl4y3r@gmail.com>
2022-01-15 12:30:14 -0700

    ArraysToDo2.js complete

--- ArrayAlgos/ArraysToDo2.js
+++ ArrayAlgos/ArraysToDo2.js
@@ -42,26 +42,39 @@
 // let leftRotate = rotateArr([1,2,3,4,5,6,7,8,9,0], 2);
 
 // Filter Range
-const filterRange = (arr, min, max) => {
-    for(let i = 0; i<arr.length; i++) {
-        console.log(arr + "is beginning")
-        if(arr[i]<=min || arr[i]>=max) {
-            if(arr[i] === arr.length-1) {
-                arr.length--;
-            }else {
-                console.log(arr);
-                for(let j = i; j<arr.length; j++) {
-                    arr[j] = arr[j+1]
-                    console.log(arr);
-                }
-                arr.length--;
-            }
-            // will start on the line after where you want to be if you let it count
-            i--;
-            console.log(arr);
-        }
+// const filterRange = (arr, min, max) => {
+//     for(let i = 0; i<arr.length; i++) {
+//         console.log(arr + "is beginning")
+//         if(arr[i]<=min || arr[i]>=max) {
+//             if(arr[i] === arr.length-1) {
+//                 arr.length--;
+//             }else {
+//                 console.log(arr);
+//                 for(let j = i; j<arr.length; j++) {
+//                     arr[j] = arr[j+1]
+//                     console.log(arr);
+//                 }
+//                 arr.length--;
+//             }
+//             // will start on the line after where you want to be if you let it count
+//             i--;
+//             console.log(arr);
+//         }
+//     }
+//     return(arr)
+// }
+// let range = filterRange([80,2,3,10,5,6,7,8,9], 2, 5);
+// console.log(range);
+
+// Concat
+const myConcat = (arr1, arr2) => {
+    let newArr = [];
+    for(let i = 0; i<arr1.length; i++) {
+        newArr.push(arr1[i]);
     }
-    return(arr)
+    for(let j = 0; j<arr2.length; j++) {
+        newArr.push(arr2[j]);
+    }
+    return newArr;
 }
-let range = filterRange([80,2,3,10,5,6,7,8,9], 2, 5);
-console.log(range);
+console.log(myConcat([1,2,3,4], ["Hi","Hello","Bye","GoodBye"]));
