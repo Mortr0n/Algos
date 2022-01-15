3c9f4f198f2dc4e984ec067cbef90db208600915
Mortron <cmsl4y3r@gmail.com>
2022-01-14 20:48:41 -0700

    Filter Range complete

--- ArrayAlgos/ArraysToDo2.js
+++ ArrayAlgos/ArraysToDo2.js
@@ -15,7 +15,7 @@
 // Rotate
 // this works, but I should really fix the negative shift one to make more sense
 // const rotateArr = (arr, shiftBy) => {
-//     if(shiftBy<0){
+//     if(shiftBy>0){
 //         for(let shift = shiftBy; shift<0; shift++) {
 //             let temp = arr[0];
 //             for(let i = arr.length-1; i >= 1; i--) {
@@ -24,7 +24,6 @@
 //                 console.log(arr);
 //             }
 //         }
-        
 //     }else {
 //         let temp = 0;
 //         for( let shift = shiftBy; shift>0; shift--)  {
@@ -37,9 +36,32 @@
 //             arr[0] = temp;
 //             console.log(arr);
 //         }
-        
 //     }
 // }
-
 // let rotate = rotateArr([1,2,3,4,5,6,7,8,9,0], 5);
-// let leftRotate = rotateArr([1,2,3,4,5,6,7,8,9,0], -2);
+// let leftRotate = rotateArr([1,2,3,4,5,6,7,8,9,0], 2);
+
+// Filter Range
+const filterRange = (arr, min, max) => {
+    for(let i = 0; i<arr.length; i++) {
+        console.log(arr + "is beginning")
+        if(arr[i]<=min || arr[i]>=max) {
+            if(arr[i] === arr.length-1) {
+                arr.length--;
+            }else {
+                console.log(arr);
+                for(let j = i; j<arr.length; j++) {
+                    arr[j] = arr[j+1]
+                    console.log(arr);
+                }
+                arr.length--;
+            }
+            // will start on the line after where you want to be if you let it count
+            i--;
+            console.log(arr);
+        }
+    }
+    return(arr)
+}
+let range = filterRange([80,2,3,10,5,6,7,8,9], 2, 5);
+console.log(range);
