566915b0c95735a1bb4df1ea1097b62310431324
Mortron <cmsl4y3r@gmail.com>
2022-01-23 20:34:36 -0700

    completed Length

--- LinkedLists/LinkedList.js
+++ LinkedLists/LinkedList.js
@@ -44,4 +44,53 @@ class LinkedList {
         return this.head.data;
     }
 
+    findSum() {      
+        // We first have to tell our train attendant that we want them to start at the front of the train
+        let runner=this.head;
+        let sum=0;  
+
+        if(this.head===null) {
+            return null;
+        }
+        
+        // Since a Linked List does not know how many nodes is within it, we will not be able to use a for loop to itereate, instead we'll use a while
+        // Also, we need to tell them when to stop otherwise they will just run off the end of the train
+        while(runner !== null){
+            // Since the runner is set to the current node, its value will be equal to the value of the node they are currently on
+            sum+=runner.data;
+            // tell our attendant to move to the next car
+            runner=runner.next;
+        }
+        return sum;
+    }
+
+    contains(value) {
+        let runner=this.head;
+        if(this.head===null) {
+            return false;
+        }
+        
+        while(runner!==null) {
+            if(value!==runner.data){
+                runner=runner.next;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    length() {
+        let runner = this.head;
+        if(this.head===null) {
+            return false;
+        }
+        let count=0;
+        while(runner!==null) {
+            count++;
+            runner = runner.next
+        }
+        return count;
+    }
+
 }
